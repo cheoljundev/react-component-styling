29b5d918f02dd086966b88bc67dc749b5e8897aa
cheoljundev <cheoljundev@gmail.com>
2024-07-10 19:13:37 +0900

    리액트 프로젝트에서 테일윈드 CSS 추가 및 사용법

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <header className="flex flex-col mt-8 items-center mb-16">
       <img src={logo} alt="A canvas" className="mb-8 w-44 h-44 object-contain"/>
-      <h1 className="text-4xl font-semibold tracking-wide text-center uppercase text-amber-800">ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <h1 className="text-4xl font-semibold tracking-wide text-center uppercase text-amber-800 font-title">ReactArt</h1>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
     </header>
   );
 }
